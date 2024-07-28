95b19ee67de197e5d63491277d20a6fb5f44cd6c
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-07-28 12:47:06 +0000

    Add database models to models.py and create database

---
+++ tool_db/models.py
@@ -0,0 +1,66 @@
+from tool_db import db
+from sqlalchemy import Text
+
+
+class MainCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    main_category_name = db.Column(db.String(100), unique=True, nullable=False)
+    sub_cats = db.relationship("SubCategory", backref="main_category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return f"{self.main_category_name}"
+    
+
+class SubCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sub_category_name = db.Column(db.String(100), unique=True, nullable=False)
+    tools = db.relationship("Tool", backref="sub_category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return f"{self.sub_category_name}"
+
+
+class Tool(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tool_name = db.Column(db.String(50), unique=True, nullable=False)
+    tool_description = db.Column(Text, nullable=False)
+    tool_videos = db.Column(db.JSON, nullable=True)
+    tool_links = db.Column(db.JSON, nullable=True)
+    main_category_id = db.Column(db.Integer, db.ForeignKey("main_category.id", ondelete="CASCADE"), nullable=False)
+    sub_category_id = db.Column(db.Integer, db.ForeignKey("sub_category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return "Name: {0} | Sub-Category: {1}".format(
+            self.tool_name, self.sub_category_id
+        )
+    
+
+class MyToolbox(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tool_name = db.Column(db.String(50), nullable=False)
+    product_links = db.Column(db.JSON, nullable=True)  # Storing up to 3 product links in JSON
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self):
+        return f"{self.tool_name}"
+
+
+class MyVideos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tool_name = db.Column(db.String(50), nullable=False)
+    video_link = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self):
+        return f"{self.tool_name}"
+
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    toolbox_items = db.relationship('MyToolbox', backref='user', cascade='all, delete', lazy=True)
+    video_items = db.relationship('MyVideos', backref='user', cascade='all, delete', lazy=True)
+
+    def __repr__(self):
+        return f"{self.username}"
--- tool_db/routes.py
+++ tool_db/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from tool_db import app, db
+from tool_db.models import MainCategory, SubCategory, Tool, User, MyToolbox, MyVideos
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
