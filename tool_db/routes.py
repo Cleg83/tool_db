a265f513f6ee0eddbf4af197c3562d290d254643
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-08-02 16:37:20 +0000

    Add step templates and integrate step functionality to add tool form

--- tool_db/routes.py
+++ tool_db/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from tool_db import app, db
 from tool_db.models import MainCategory, SubCategory, Tool, User, MyToolbox, MyVideos
 
@@ -83,8 +83,47 @@ def sub_category_page(sub_category_id):
 @app.route("/add_tool", methods=["GET", "POST"])
 def add_tool():
     main_categories = MainCategory.query.all()
-    sub_categories = SubCategory.query.all()
-    return render_template("add_tool.html", main_categories=main_categories, sub_categories=sub_categories)
+
+    if request.method == "POST":
+        step = request.form.get('step')
+
+        if step == "1":
+            # Store the tool details in session
+            session['tool_name'] = request.form.get('tool_name')
+            session['tool_description'] = request.form.get('tool_description')
+            session['tool_videos'] = request.form.get('tool_videos')
+            session['product_links'] = request.form.get('product_links')
+            return render_template("add_tool_step2.html", main_categories=main_categories)
+        
+        elif step == "2":
+            # Store the main category selection in session
+            session['main_category_id'] = request.form.get('main_category')
+            # Filter sub-categories based on main category
+            subcategories = SubCategory.query.filter_by(main_category_id=session['main_category_id']).all()
+            return render_template("add_tool_step3.html", subcategories=subcategories)
+        
+        elif step == "3":
+            # Store the sub-category selection in session
+            sub_category_id = request.form.get('sub_category')
+
+            # Create a new tool and save to the database
+            new_tool = Tool(
+                tool_name=session['tool_name'],
+                tool_description=session['tool_description'],
+                tool_videos=session['tool_videos'],
+                tool_links=session['product_links'],
+                main_category_id=session['main_category_id'],
+                sub_category_id=sub_category_id
+            )
+            db.session.add(new_tool)
+            db.session.commit()
+
+            # Clear the session
+            session.clear()
+
+            return redirect(url_for('categories'))
+    
+    return render_template("add_tool_step1.html")
 
 
 @app.route("/edit_tool")
