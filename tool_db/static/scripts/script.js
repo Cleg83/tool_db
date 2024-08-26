7f1bfcc387ea90e552c5a7e14f27f18b72388570
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-08-26 16:59:24 +0000

    Remove unecessary JS validation for password validation now flash messages are displaying correctly

--- tool_db/static/scripts/script.js
+++ tool_db/static/scripts/script.js
@@ -5,13 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
   });
-
-  document.getElementById('register_form').addEventListener('submit', function(event) {
-    const password = document.getElementById('password').value;
-    const passwordConfirm = document.getElementById('password_confirm').value;
-    
-    if (password !== passwordConfirm) {
-        event.preventDefault(); // Prevent form submission
-        alert('Passwords do not match!');
-    }
-});
