5d2a8b128bca1867fdefec516dc9662c3831c0de
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-09-03 09:23:04 +0000

    Add sidenav collapse to navbar

--- tool_db/static/scripts/script.js
+++ tool_db/static/scripts/script.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let select = document.querySelectorAll('select');
     M.FormSelect.init(select);
 
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+
   });
