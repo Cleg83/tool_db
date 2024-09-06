751e783d5595f1e27819f624a2ed4c1d2032f7a8
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-09-06 18:56:54 +0000

    Simplify fetch random tool function. Add lighthouse scores. Add more css including media queries to resolve display issues

--- tool_db/static/scripts/script.js
+++ tool_db/static/scripts/script.js
@@ -1,70 +1,78 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let select = document.querySelectorAll('select');
-    M.FormSelect.init(select);
+  // Initialise components
 
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal, {});
 
-    let modal = document.querySelectorAll('.modal');
-    M.Modal.init(modal);
+  let select = document.querySelectorAll('select');
+  M.FormSelect.init(select);
 
-  });
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
 
-  function fetchRandomTool() {
-    fetch('/random_tool')
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          console.error(data.error);
-          return;
-        }
 
-        // Update card content
-        document.getElementById('tool-name').textContent = data.tool_name;
-        document.getElementById('tool-description').textContent = data.tool_description;
 
-        // Update videos
-        let toolVideos = document.getElementById('tool-videos');
-        toolVideos.innerHTML = '';
-        if (data.tool_videos) {
-          data.tool_videos.forEach(video => {
-            let embedUrl = video.replace("watch?v=", "embed/");
-            toolVideos.innerHTML += `<iframe width="426" height="240" src="${embedUrl}" frameborder="0" allowfullscreen></iframe>`;
-          });
-        }
+  // Initial fetch
+  if (document.body.classList.contains('home-page')) {
+      fetchRandomTool();
+      
+      // Fetch new random tool every 10 seconds
+      fetchInterval = setInterval(fetchRandomTool, 10000);
+  }
+});
 
-        // Update links
-        let toolLinks = document.getElementById('tool-links');
-        toolLinks.innerHTML = '';
-        if (data.tool_links) {
-          data.tool_links.forEach(link => {
-            toolLinks.innerHTML += `<span class="home-page-links"><a href="${link}" target="_blank">Buy here</a></span>`;
-          });
-        }
+function fetchRandomTool() {
+  fetch('/random_tool')
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        console.error(data.error);
+        return;
+      }
 
-        // Update Add to My Toolbox button
-        let toolAction = document.getElementById('tool-action');
-        if (data.is_authenticated) {
-          // Construct the dynamic URL for the form action
-          const addToToolboxUrl = `/add_to_my_toolbox/${data.tool_id}`;
-          
-          toolAction.innerHTML = `
-            <form action="${addToToolboxUrl}" method="post">
-              <button type="submit" class="edit-small">
-                <i class="fa-solid fa-toolbox"></i>
-                <small class="small-icon-text">Add to My Toolbox</small>
-              </button>
-            </form>
-          `;
-        } else {
-          toolAction.innerHTML = ''; // Clear if not authenticated
-        }
-      })
-      .catch(error => console.error('Error fetching random tool:', error));
+      // Get the random tool card
+      const randomToolCard = document.getElementById('random-tool-card');
+      if (randomToolCard) {
+          // Apply fade-out effect before updating content
+          randomToolCard.classList.add('fade-out');
+          randomToolCard.classList.remove('show');
+
+          setTimeout(() => {
+              // Update tool name with a link to the tool's page
+              const toolLink = `<a href="/tool/${data.tool_id}" class="home-card-link">${data.tool_name}</a>`;
+              document.getElementById('tool-name').innerHTML = toolLink;
+
+              document.getElementById('tool-description').textContent = data.tool_description;
+
+              // Update Add to My Toolbox button
+              let toolAction = document.getElementById('tool-action');
+              if (data.is_authenticated) {
+                // Construct the dynamic URL for the form action
+                const addToToolboxUrl = `/add_to_my_toolbox/${data.tool_id}`;
+                
+                toolAction.innerHTML = `
+                  <form action="${addToToolboxUrl}" method="post" class="home-toolbox">
+                    <button type="submit" class="edit-small borderless-btn">
+                      <i class="fa-solid fa-toolbox"></i>
+                      <small class="small-icon-text">Add to My Toolbox</small>
+                    </button>
+                  </form>
+                `;
+              } else {
+                toolAction.innerHTML = ''; // Clear if not authenticated
+              }
+
+              // Remove fade-out and reapply fade-in effect
+              randomToolCard.classList.remove('fade-out');
+              randomToolCard.classList.add('show');
+          }, 250); 
+      }
+    })
+    .catch(error => console.error('Error fetching random tool:', error));
 }
 
-// Initial fetch
-fetchRandomTool();
-
-// Fetch new random tool every 7.5 seconds
-setInterval(fetchRandomTool, 7500);
+window.addEventListener('beforeunload', function() {
+  if (fetchInterval) {
+      clearInterval(fetchInterval);
+  }
+});
